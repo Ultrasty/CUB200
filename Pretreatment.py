96322a606fac9fea917c7764612e9a4fa3d1ccfc
青袍君 <51046846+Ultrasty@users.noreply.github.com>
2021-01-18 21:38:16 +0800

    数据清洗由delete_掐头去尾完成，不由本文件负责了

--- Pretreatment.py
+++ Pretreatment.py
@@ -17,36 +17,34 @@ while i < 80:
     k = 0
     if i < 55:
         for j in list[i]:
-            if 20 < k:
-                if j == '1':
-                    try:
-                        shutil.copyfile('./Covid-19/P' + str(1000 + i + 1)[1:] + '/2-CT' + str(10000 + k)[1:] + '.png',
-                                        './theCovid-19/' + str(count) + '.png')
-                    except Exception:
-                        pass
-                if j == '0':
-                    try:
-                        shutil.copyfile('./Covid-19/P' + str(1000 + i + 1)[1:] + '/2-CT' + str(10000 + k)[1:] + '.png',
-                                        './theNormal/' + str(count) + '.png')
-                    except Exception:
-                        pass
+            if j == '1':
+                try:
+                    shutil.copyfile('./Covid-19/P' + str(1000 + i + 1)[1:] + '/2-CT' + str(10000 + k)[1:] + '.png',
+                                    './theCovid-19/' + str(count) + '.png')
+                except Exception:
+                    pass
+            if j == '0':
+                try:
+                    shutil.copyfile('./Covid-19/P' + str(1000 + i + 1)[1:] + '/2-CT' + str(10000 + k)[1:] + '.png',
+                                    './theNormal/' + str(count) + '.png')
+                except Exception:
+                    pass
             k = k + 1
             count = count + 1
     else:
         for j in list[i]:
-            if 20 < k:
-                if j == '1':
-                    try:
-                        shutil.copyfile('./Cap/cap' + str(1000 + i - 54)[1:] + '/2-CT' + str(10000 + k)[1:] + '.png',
-                                        './theCap/' + str(count) + '.png')
-                    except Exception:
-                        pass
-                if j == '0':
-                    try:
-                        shutil.copyfile('./Cap/cap' + str(1000 + i - 54)[1:] + '/2-CT' + str(10000 + k)[1:] + '.png',
-                                        './theNormal/' + str(count) + '.png')
-                    except Exception:
-                        pass
+            if j == '1':
+                try:
+                    shutil.copyfile('./Cap/cap' + str(1000 + i - 54)[1:] + '/2-CT' + str(10000 + k)[1:] + '.png',
+                                    './theCap/' + str(count) + '.png')
+                except Exception:
+                    pass
+            if j == '0':
+                try:
+                    shutil.copyfile('./Cap/cap' + str(1000 + i - 54)[1:] + '/2-CT' + str(10000 + k)[1:] + '.png',
+                                    './theNormal/' + str(count) + '.png')
+                except Exception:
+                    pass
             k = k + 1
             count = count + 1
 
